d6b87f32d6e8351f90e76d54150d53c073711491
Екатерина <katya.borodich@list.ru>
2023-06-22 14:26:42 +0300

    profile

--- app/src/main/java/com/example/bookswap/MainActivity.kt
+++ app/src/main/java/com/example/bookswap/MainActivity.kt
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth = Firebase.auth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<LinearLayout>(R.id.account_image_layout).setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
+            val intent: Intent = if (auth.currentUser != null) {
+                Intent(this, UserProfileActivity::class.java)
+            } else {
+                Intent(this, SignInActivity::class.java)
+            }
             startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/bookswap/UserProfileActivity.kt
+++ app/src/main/java/com/example/bookswap/UserProfileActivity.kt
@@ -1,11 +1,54 @@
 package com.example.bookswap
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.PopupMenu
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class UserProfileActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth = Firebase.auth
+
+    @SuppressLint("DiscouragedPrivateApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        findViewById<ImageView>(R.id.popup).setOnClickListener{
+            val popupMenu = PopupMenu(this, it)
+            popupMenu.setOnMenuItemClickListener { item ->
+                when(item.itemId) {
+                    R.id.popup_edit -> {
+                        //val intent = Intent()
+                        true
+                    }
+                    R.id.popup_logout -> {
+                        auth.signOut()
+                        finish()
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popupMenu.inflate(R.menu.menu_profile)
+
+            try{
+                val fieldMPopup = PopupMenu::class.java.getDeclaredField("mPopup")
+                fieldMPopup.isAccessible = true
+                val mPopup = fieldMPopup.get(popupMenu)
+                mPopup.javaClass.getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                    .invoke(mPopup, true)
+            } catch (e: Exception) {
+                Log.e("error", "Error showing menu icons!", e)
+            } finally {
+                popupMenu.show()
+            }
+        }
     }
 }
